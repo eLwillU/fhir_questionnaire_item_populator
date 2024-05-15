b22b6f250dcc5d001de11a80d356c4e15365f902
Dominic Willi <dominic.willi@students.bfh.ch>
2024-05-15 17:05:24 +0200

    updated script

--- main.py
+++ main.py
@@ -1,16 +1,124 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+import json
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+def createquetiontitle(linkid, prefix, question_text_de, question_text_fr):
+    templatequestiontitle = {
+        "linkId": linkid,
+        "prefix": prefix,
+        "text": question_text_de,
+        "_text": {
+            "extension": [
+                {
+                    "url": "http://hl7.org/fhir/StructureDefinition/translation",
+                    "extension": [
+                        {
+                            "url": "lang",
+                            "valueCode": "de"
+                        },
+                        {
+                            "url": "content",
+                            "valueString": question_text_de
+                        }
+                    ]
+                },
+                {
+                    "url": "http://hl7.org/fhir/StructureDefinition/translation",
+                    "extension": [
+                        {
+                            "url": "lang",
+                            "valueCode": "fr"
+                        },
+                        {
+                            "url": "content",
+                            "valueString": question_text_fr
+                        }
+                    ]
+                }
+            ]
+        },
+        "type": "choice",
+        "required": True,
+        "answerOption": []
+    }
+    return templatequestiontitle
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def createansweroption(answer_text_de, answer_text_fr, code):
+    answeroption = {
+        "valueCoding": {
+            "code": code,
+            "display": answer_text_de,
+            "_display": {
+                "extension": [
+                    {
+                        "url": "http://hl7.org/fhir/StructureDefinition/translation",
+                        "extension": [
+                            {
+                                "url": "lang",
+                                "valueCode": "de"
+                            },
+                            {
+                                "url": "content",
+                                "valueString": answer_text_de
+                            }
+                        ]
+                    },
+                    {
+                        "url": "http://hl7.org/fhir/StructureDefinition/translation",
+                        "extension": [
+                            {
+                                "url": "lang",
+                                "valueCode": "fr"
+                            },
+                            {
+                                "url": "content",
+                                "valueString": answer_text_fr
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+    return answeroption
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def writetojson(jsondata):
+    with open('data.json', 'a', encoding='utf-8') as f:
+        # Serialize the data and write it to the file
+        json.dump(jsondata, f, ensure_ascii=False, indent=4)
+
+
+df = pd.read_excel('data/data.xlsx',
+                   dtype={"linkid": int,
+                          "prefix": int,
+                          "numAnswers": int},
+                   na_values=["NA"])
+res = []
+options = []
+
+for index, row in df.iterrows():
+    juan = createquetiontitle(linkid=row["linkid"], prefix=row["prefix"], question_text_de=row["question_text_de"],
+                              question_text_fr=row["question_text_fr"])
+
+    german_value = ""
+    french_value = ""
+    counter = 0
+    num_answers_value = row["numAnswers"]
+
+    for i, (german, french) in enumerate(zip(row[6:], row.iloc[6 + num_answers_value:])):
+        if i == num_answers_value:
+            break
+        if pd.notna(german):
+            german_value = german
+        if pd.notna(french):
+            french_value = french
+        juan_son = createansweroption(answer_text_de=german_value, answer_text_fr=french_value, code=counter)
+        counter += 1
+        options.append(juan_son)
+
+    juan["answerOption"].extend(options)
+    res.append(juan)
+
+writetojson(res)
